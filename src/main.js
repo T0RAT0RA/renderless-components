6e69bb17d728f5d05487ee986342799765e0f1ef
T0RAT0RA <stephane.reiss@gmail.com>
2020-10-25 17:58:06 -0400

    Add composition API

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueCompositionAPI from "@vue/composition-api";
+
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
